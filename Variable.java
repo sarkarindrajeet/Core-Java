5c42d3dc95efdef6f1e46fd282e64b4c427b2891
Indrajeet <sarkarindrajeet32@gmail.com>
2021-05-23 23:48:59 +0530

    Variable

---
+++ Variable.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author jeetsarkar
+ */
+//How to create Variable 
+public class Variable {
+    public static void main(String[] args) {
+        
+        int a=100; //here int is DataType and ( a is name of variable) 100 is value of a.
+        
+        System.out.println("Print the value of a = "+a); // this line is print the value of a.
+    }
+    
+    
+}
