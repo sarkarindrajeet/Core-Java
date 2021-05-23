115a055204951e23b403651fd30e0e72d4088d9e
Indrajeet <sarkarindrajeet32@gmail.com>
2021-05-23 22:51:49 +0530

    Initial commit

---
+++ Hello_World.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+// This is my first program. Print Hello World.
+package hello_world;
+
+
+public class Hello_World { // this is a class and Hello_World is class name .
+
+    
+    public static void main(String[] args) { // it is a main function of program.
+        
+        System.out.println("Hello World"); // it is a print line 
+    }
+    
+}
